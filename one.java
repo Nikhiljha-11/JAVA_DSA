c96b0e38712b5eb4f6a467252515f969e0663fe6
Nikhil Jha <nikhiljha8825@gmail.com>
2026-02-09 22:31:09 +0530

    Add files via upload

---
+++ one.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;;
+public class one {
+    public static void main (String []args){
+        int num1 ,num2,num3;
+        num1=1;
+        num2=2;
+         String name1,name2;
+         name1="NIKHIL";
+         name2="\tJHA";
+         System.out.println(name1 + name2);
+
+
+
+
+    }
+    
+}
