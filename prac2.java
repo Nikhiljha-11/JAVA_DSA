b25f57e92671e363925cecb547fbd30e3e86ad5b
Nikhil Jha <nikhiljha8825@gmail.com>
2026-03-05 22:59:53 +0530

    Add files via upload

---
+++ prac2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class prac2
+{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+         System.out.println("Hii");
+         System.out.println(4+8);
+         String name =  sc.nextLine();
+         System.out.println("your name is" + name);
+    }
+}
