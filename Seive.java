654ec752719ec92ff344f3471d4c4ae82bd34f3a
Nikhil Jha <nikhiljha8825@gmail.com>
2026-03-14 18:36:47 +0530

    Remove unnecessary line breaks in Seive.java

--- Seive.java
+++ Seive.java
@@ -6,7 +6,6 @@ public class Seive {
         boolean[] primes = new boolean[n+1];
         sieve(n, primes);
     }
-
     // false in array means number is prime
     static void sieve(int n, boolean[] primes) {
         for (int i = 2; i*i <= n; i++) {
