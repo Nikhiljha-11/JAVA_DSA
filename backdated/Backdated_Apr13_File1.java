65e12e7d25239f56c2c6113486eeb1bed88e1bff
NIKHIL JHA <nikhiljha8825@gmail.com>
2026-04-13 10:00:00 +0530

    Add backdated/Backdated_Apr13_File1.java

---
+++ backdated/Backdated_Apr13_File1.java
@@ -0,0 +1,12 @@
+public class Backdated_Apr13_File1 {
+    static int factorial(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(factorial(5));
+    }
+}
