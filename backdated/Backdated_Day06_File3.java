847088b1cf1cebf2c7c1f07223770b787abedf37
NIKHIL JHA <nikhiljha8825@gmail.com>
2026-05-06 11:00:00 +0530

    Add Backdated_Day06_File3.java

---
+++ backdated/Backdated_Day06_File3.java
@@ -0,0 +1,6 @@
+﻿public class Day06File3 {
+    private int x;
+    public Day06File3(int x) { this.x = x; }
+    public int getX() { return x; }
+    public static void main(String[] args) { System.out.println(new Day06File3( 15 ).getX()); }
+}
